4f379182baddd4b3d56aff976bb82f6d17669ff8
Celine Tran <celinephoungtran@gmail.com>
2023-09-23 22:41:48 -0400

    added techstack for each card

--- src/components/WorkCard.jsx
+++ src/components/WorkCard.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
 export default function WorkCard ({data}) {
+    const techStack = data.techStack.map(tech => 
+        <div className="text-sm inline-flex justify-center items-center px-2 py-1 rounded-md bg-pink-300">{tech}</div>
+    );
+
     return (
-        <div className="container rounded-md flex flex-row justify-around items-center gap-4 p-4 bg-pink-200">
+        <div className="container rounded-md flex flex-row justify-around items-center gap-4 px-4 py-6 bg-pink-200">
             <img 
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHN8VZBh3H-DJG7Cp3kfbRDnd7UF932qrhJMVqjA7uJw&s" 
                 alt="" 
@@ -11,10 +15,9 @@ export default function WorkCard ({data}) {
             <div className="flex flex-col">
                 <p className="text-sm">{data.company}</p>
                 <h1 className="text-2xl uppercase font-bold">{data.title}</h1>
-                {/* <div className="flex justify-around">
-                    <div>Python</div>
-                    <div>HTML</div>
-                </div> */}
+                <div className="flex flex-wrap gap-2 my-2">
+                    {techStack}
+                </div>
                 <p>{data.description}</p>
             </div>
         </div>
