743891a174e7477b5ff792d2aab75788596b05ac
Celine Tran <celinephoungtran@gmail.com>
2023-09-25 15:29:02 -0400

    Add react routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import './App.css'
-
+import { Routes, Route } from "react-router-dom"
 import Home from './Pages/Home'
 
 function App() {
   return (
     <>
-        <Home></Home>
+    <Routes>
+        <Route path="/" element={ <Home /> } />
+    </Routes>
     </>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
-    import React, { useState } from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
@@ -13,9 +14,9 @@ export default function NavBar () {
                 <FontAwesomeIcon icon={isOpen ? faXmark : faBars} size="xl" />
             </button>
             <nav className={`${isOpen ? "flex" : "hidden"} md:flex flex-col md:flex-row md:justify-end items-end md:items-center gap-4 md:gap-8 md:px-16 md:py-4 md:h-16`}>
-                <a href="" className="hover:underline">HOME</a>
-                <a href="" className="hover:underline">PROJECTS</a>
-                <a href="" className="hover:underline">CONTACT</a>
+                <Link to={"/"} className="uppercase hover:underline">HOME</Link>
+                <Link className="uppercase hover:underline">PROJECTS</Link>
+                <Link className="uppercase hover:underline">CONTACT</Link>
             </nav>
         </header>
     );
