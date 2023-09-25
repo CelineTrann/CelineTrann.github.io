ff053e17fed3d3647f4950f24d4c4bf4b3128f33
Celine Tran <celinephoungtran@gmail.com>
2023-09-25 16:04:37 -0400

    created project information

---
+++ src/data/ProjectInfo.js
@@ -0,0 +1,61 @@
+export const ProjectInfo = [{
+    name: "Twitter Clone",
+    description: "Created a fully functional Twitter clone using Django. This application allows users to create an account and a custom profile with a cover and profile picture. The site supports following/unfollowing other users, and creating, liking, retweeting, replying and deleting tweets. Users are also able to search for user and posts, as well as change their username and password. Additionally, users can explore the profile page of other users where they can view their tweets and likes, as well as their follower/following.",
+    techStack: ["django", "HTML", "CSS", "JQuery", "SQLLite"],
+    links: ["https://github.com/CelineTrann/twitter_clone"]
+},
+{
+    name: "Portfolio Website",
+    description: "Create a personal website to showcase previous work experiences and projects",
+    techStack: ["React", "React Router", "Vite"],
+    links: ["https://github.com/CelineTrann/CelineTrann.github.io-V2", ]
+},
+{
+    name: "Netflix UI Clone",
+    description: "Created the Netflix UI",
+    techStack: ["React", "SASS", "Vite"],
+    links: ["https://github.com/CelineTrann/Netflix_Clone"]
+},
+{
+    name: "To Do App",
+    description: "A basic to do application that allows users to create task and mark them as complete. It uses local storage to keep track of the created tasks.",
+    techStack: ["React", "Vite"],
+    links: ["https://github.com/CelineTrann/ToDoApp"]
+},
+{
+    name: "Workout Watcher",
+    description: "Created a smart yoga mat that is able to determine the users yoga pose correctness based on hand/feet pressure and orientation. This was done to help users correct their yoga poses when doing them alone. The system uses image processing techniques to differentiate between body parts, as well as analyze the pressure data recieved from the yoga mat sensors.",
+    techStack: ["Python", "OpenCV", "Raspberry Pi", "Pressure Sensors"],
+    links: ["https://github.com/CelineTrann/Workout_Watcher", ]
+},
+{
+    name: "Sudoku Solver",
+    description: "Implemented a recursive algorithm that solves Sudoku puzzles",
+    techStack: ["Python", ],
+    links: ["https://github.com/CelineTrann/Sudoku_solver"]
+},
+{
+    name: "Ao3 Stats",
+    description: "Webscrapper designed to count the number of works created per fandom on the website archiveofourown.com",
+    techStack: ["Python", "BeautifulSoup"],
+    links: ["https://github.com/CelineTrann/ao3_stats"]
+},
+{
+    name: "Vidle",
+    description: "Created a MVC used to keep track of movie rentals.",
+    techStack: [".NET Framework", "CSS", "HTML", "Bootstrap"],
+    links: ["https://github.com/CelineTrann/Vidly"]
+},
+{
+    name: "Ritual Rewind",
+    description: "Created an interactive review of user's statistics in order to encourage users to continue to use the Ritual application for food ordering.",
+    techStack: ["React", "Styled Components", "Figma"],
+    links: []
+},
+{
+    name: "Find Space",
+    description: "Using a web camera and a Dragon Board, Find Space is able to detect empty parking lots and notify drivers, via a light, that a parking spot is available at that location.",
+    techStack: ["Python", "Microsoft Azure Computer Vision", "Dragon Board"],
+    links: ["https://github.com/tristanle22/FindSpace"]
+},
+];
