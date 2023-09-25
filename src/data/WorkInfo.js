4b043bd1943fd2451ea9d74eb6de73f3180e1e92
Celine Tran <celinephoungtran@gmail.com>
2023-09-25 13:19:47 -0400

    Updated work info with all information

--- src/data/WorkInfo.js
+++ src/data/WorkInfo.js
@@ -11,4 +11,26 @@ export const workInfo = [{
     title: "Java Developer",
     description: "At Paramount Commerce, I implemented logging into the microservice using LogBack for DataDogs monitoring. I also solved serverside issues relating to the Postgres database, the message queue, and the API specification. I also implemented unit tests using JUnit, Mockito, and WireMock.",
     techStack: ["MVC Spring Boot", "Postgres", "Liquidbase", "RabbitMQ", "OpenAPI", "Swagger"]
-}];
+},
+{
+    company: "Telus Health",
+    location: "Cambridge, ON",
+    title: "Software Developer in Test",
+    description: "I created over 100 test cases that performed UI and regression testing on a the PS Suite EMR. Using Selenium, Robot Framework, and Froglogic Squish. Additionally, I maintained over 800 test cases and ensured the successful build on the project on Jenkins. I was also able to optimizing a test suite to reduce its run time from 8 hours to 2 hours without sacrificing any testing functionalities.",
+    techStack: ["Python3", "Selenium", "Robot Framework", "Froglogic Squish"]
+},
+{
+    company: "Ritual Co.",
+    location: "Toronto, ON",
+    title: "Quality Assurance Analyst",
+    description: "I created and executed manual test plans on the user onboarding flow, ordering flow, and the Piggyback system for various mobile and desktop systems. This included creating UI, functional, smoke, and regression tests. During testing I used SQL and GCP to explore correct data integrity and network errors that would occur. I also created an initialive to track allocation of the QA team's resources to improve resource sharing amongst the engineering team. Additionally, I also setup and created UI tests to test the onboarding and ordering flow on the website using Selenium and Robot Framework.",
+    techStack: ["Test Rail", "Postman", "Charles Proxy", "GCP", "Robot Framework", "Selenium"]
+},
+{
+    company: "ShopEdge",
+    location: "Kitchener, ON",
+    title: "Software Developer",
+    description: "At ShopEdge I maintained both the desktop application using .NET Framework, and the Android application using Java for a ERP software. I also created custom Crystal Reports and SSRS reports based on customer requirements. Additionally, I created and maintained stored procedures used in the applicaton using Microsoft SQL Server. ",
+    techStack: [".NET Framework", "Android Studio", "Java", "Microsoft SQL Server", "SSRS Reports", "Crystal Reports"]
+},
+];
